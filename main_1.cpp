cd00d523236d7de0795bdc3a88dafc60f75547af
AnnSolovey <125373673+AnnSolovey@users.noreply.github.com>
2023-02-19 13:11:11 +0300

    Add files via upload

---
+++ main_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Triangle.h"
+
+using namespace std;
+
+int main() {
+	Triangle mass[3];
+	double a, b, c;
+	for (int i = 0; i < 3; i++) {
+		cout << "Enter a, b and c for the triangle #" << i + 1 << " separated by a space: " << endl;
+		cin >> a >> b >> c;
+		cout << endl;
+		mass[i].set(a, b, c);
+		if (!(mass[i].exst_tr())) {
+			mass[i].show();
+			cout << "There is no triangle with such sides, try again!" << endl << endl;
+			i--;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		mass[i].show();
+		cout << "Perimeter of the triangle: " << mass[i].perimetr() << endl;
+		cout << "Square of the triangle: " << mass[i].square() << endl << endl;
+	}
+}
